8d363365d33689fdd234f2f6db38a0f8f2d636d2
Alexey Machihine <alesha.machihine2005@mail.ru>
2024-10-30 18:14:16 +0300

    Add files

---
+++ SeleniumInitialize_Tests/TestBaseSetUp.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using SeleniumInitialize_Builder;
+
+namespace SeleniumInitialize_Tests
+{
+    public abstract class TestBaseSetUp
+    {
+        public SeleniumBuilder Builder;
+        public Browser Browser;
+
+        [SetUp]
+        public void Setup()
+        {
+            Builder = new SeleniumBuilder();
+        }
+        [TearDown]
+        public void Dispose()
+        {
+            if ((TestContext.CurrentContext.Result.Outcome == ResultState.Failure) ||
+            (TestContext.CurrentContext.Result.Outcome == ResultState.Error))
+                Builder.Browser.TackeScreenshot();
+            Builder.Dispose();
+        }
+    }
+}
