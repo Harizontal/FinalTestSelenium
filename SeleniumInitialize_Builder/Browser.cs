375a1215f78fe8980d525a7651468c69b6b4371f
Harizontal <94596912+Harizontal@users.noreply.github.com>
2024-10-30 18:12:04 +0300

    Add files

---
+++ SeleniumInitialize_Builder/Browser.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumInitialize_Builder
+{
+    public class Browser
+    {
+        private IWebDriver _driver;
+        public Browser(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        /// <summary>
+        /// Метод для ожидание загрузки страницы
+        /// </summary>
+        public void WaitForPageLoad(By element)
+        {
+            Thread.Sleep(500);
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(5))
+                 .Until(ExpectedConditions
+                 .ElementIsVisible(element));
+        }
+        /// <summary>
+        /// Метод создаёт скриншот экрана страницы при падение теста
+        /// </summary>
+        public void TackeScreenshot()
+        {
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+            var timeStamp = DateTime.Now.ToString("yyyy_dd_MM_HH_mm");
+            var fileName = $"Failed_{timeStamp}.png";
+            var relativePath = Path.Combine("Screenshot", fileName);
+            if(!Directory.Exists(relativePath))
+                Directory.CreateDirectory(Path.GetDirectoryName(relativePath));
+            screenshot.SaveAsFile(relativePath);
+        }
+
+        public void OpenTab(string URL)
+        {
+            ((IJavaScriptExecutor)_driver).ExecuteScript($"window.open('" + URL + "')");
+        }
+        /// <summary>
+        /// Метод переключает на выбранную вкладку по индексу
+        /// </summary>
+        public void SwitchToTab(int index)
+        {
+            _driver.SwitchTo().Window(_driver.WindowHandles[index]);
+        }
+        /// <summary>
+        /// Метод закрывает текущую вкладку и переключается по последнему дескриптору
+        /// </summary>
+        public void Close()
+        {
+            _driver.Close();
+            _driver.SwitchTo().Window(_driver.WindowHandles[_driver.WindowHandles.Count - 1]);
+        }
+    }
+}
---
+++ SeleniumInitialize_Builder/SeleniumBuilder.cs
@@ -0,0 +1,112 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumInitialize_Builder
+{
+    public class SeleniumBuilder : IDisposable
+    {
+        private IWebDriver WebDriver { get; set; }
+        public int Port { get; private set; }
+        public bool IsDisposed { get; private set; }
+        public List<string> ChangedArguments { get; private set; }
+        public Dictionary<string, object> ChangedUserOptions { get; private set; }
+        public TimeSpan Timeout { get; private set; }
+        public string StartingURL { get; private set; }
+        public Browser Browser { get; private set; }
+        public bool IsHeadless
+        {
+            get
+            { return ChangedArguments != null && ChangedArguments.Contains("--headless"); }
+        }
+
+        public IWebDriver Build()
+        {
+            var service = ChromeDriverService.CreateDefaultService();
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("download.default_directory", @"C:\Downloads");
+            options.AddUserProfilePreference("plugins.always_open_pdf_externally", true);
+            if (Port != default)
+                service.Port = Port;
+            if (ChangedArguments != null)
+                options.AddArguments(ChangedArguments);
+            if (ChangedUserOptions != null)
+            {
+                foreach (var opt in ChangedUserOptions)
+                {
+                    options.AddAdditionalOption(opt.Key, opt.Value);
+                }
+            }
+            WebDriver = new ChromeDriver(service, options);
+            WebDriver.Manage().Timeouts().ImplicitWait = Timeout;
+            if (!string.IsNullOrEmpty(StartingURL))
+                WebDriver.Navigate().GoToUrl(StartingURL);
+            Browser = new Browser(WebDriver);
+            return WebDriver;
+        }
+
+        public void Dispose()
+        {
+            //Закрыть браузер, очистить использованные ресурсы, по завершении переключить IsDisposed на состояние true
+            WebDriver?.Quit();
+            WebDriver?.Dispose();
+            IsDisposed = true;
+        }
+
+        public SeleniumBuilder ChangePort(int port)
+        {
+            //Реализовать смену порта, на котором развёрнут IWebDriver для этого необходимо ознакомиться с классом DriverService соответствующего драйвера (ChromeDriverService для хрома)
+            //Изменить свойство Port на тот порт, на который поменяли.
+            //Builder в данном методе должен возвращать сам себя
+            Port = port;
+            return this;
+        }
+
+        public SeleniumBuilder SetArgument(string argument)
+        {
+            //Реализовать добавление аргумента. При решении данной задаче ознакомитесь с классом Options соответствующего драйвера (ChromeOptions для браузера Chrome)
+            //Все изменённые/добавленные аргументы необходимо отразить в свойстве СhangedArguments, которое перед этим нужно где-то будет проинициализировать.
+            //Builder в данном методе должен возвращать сам себя
+            ChangedArguments ??= new List<string>();
+            ChangedArguments.Add(argument);
+            return this;
+        }
+
+        public SeleniumBuilder SetUserOption(string option, object value)
+        {
+            //Реализовать добавление пользовательской настройки.
+            //Все изменения сохранить в свойстве ChangedUserOptions
+            //Builder в данном методе должен возвращать сам себя
+            ChangedUserOptions ??= new Dictionary<string, object>();
+            if (!ChangedUserOptions.ContainsKey(option))
+                ChangedUserOptions.Add(option, value);
+            return this;
+        }
+
+        public SeleniumBuilder EditHeadless()
+        {
+            if (IsHeadless)
+                ChangedArguments?.Remove("--headless");
+            else
+                ChangedArguments = ChangedArguments?.Append("--headless").ToList() ?? new List<string> { "--headless" };
+            return this;
+        }
+
+        public SeleniumBuilder WithTimeout(TimeSpan timeout)
+        {
+            //Реализовать изменение изначального таймаута запускаемого браузера
+            //Отслеживать изменения в свойстве Timeout
+            //Builder возвращает себя
+            Timeout = timeout;
+            return this;
+        }
+
+        public SeleniumBuilder WithURL(string url)
+        {
+            //Реализовать изменения изначального URL запускаемого браузера
+            //Отслеживать изменения в строке StartingURL
+            //Builder возвращает себя
+            StartingURL = url;
+            return this;
+        }
+    }
+}
